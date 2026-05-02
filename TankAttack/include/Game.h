caf5e1c8215b17b800f1eba325cf7ca2ce42394f
Alanna Mendoza <alanna.itz27@gmail.com>
2026-05-02 00:52:07 -0600

    [ADD] Menu del juego

---
+++ TankAttack/include/Game.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+enum class GameState {
+	menu,
+	playing,
+	exit
+};
+
+class Game {
+
+private:
+
+	// configuraciones basicas de la ventana
+	void initVariables();
+	void initWindow();
+
+	// ATRIBUTOS
+
+	// window
+	sf::RenderWindow* windowGame;
+	sf::Event gameEvent;
+	sf::VideoMode videoMode;
+
+	// estado del juego
+	GameState State;
+
+	// ATRIBUTOS DEL MENU
+	
+	// fondos del juego
+	sf::Texture background;
+	sf::Sprite backgroundMenu;
+
+	// fuente principal
+	sf::Font font;
+
+	//botones y texto
+	sf::RectangleShape playButton;
+	sf::RectangleShape closeButton;
+	sf::Text playText;
+	sf::Text closeText;
+
+	// ATRIBUTOS DEL JUEGO
+	sf::RectangleShape backButton;
+	sf::Text backText;
+	bool GameInit = false;
+
+
+
+
+public:
+	// constructor y destructor
+	Game();
+	~Game();
+
+	// acceder a variables
+	const bool getWindowOpen() const;
+
+	// metodos del juego
+	void run();
+	void PollEvents();
+	void update();
+	void render();
+
+	// metodos del menu
+	void initMenu();
+	void updateMenu();
+	void renderMenu();
+
+	// metodos del juego
+	void initGame();
+	void updateGame();
+	void renderGame();
+
+
+
+};
--- TankAttack/src/Main.cpp
+++ TankAttack/src/Main.cpp
@@ -1,27 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main() {
-    // Creamos una ventana de 800x600 con un título
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML 2.6 Funcionando!");
+    Game game;
 
-    // Creamos un círculo verde
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Magenta);
-    shape.setPosition(300, 200);
+    while (game.getWindowOpen()) {
+        
+        // update juego
+        game.update();
 
-    // Bucle principal
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // Si se cierra la ventana
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Limpiar, dibujar y mostrar
-        window.clear();
-        window.draw(shape);
-        window.display();
+        //render juego
+        game.render();
     }
 
     return 0;
